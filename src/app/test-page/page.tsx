95bc5a362c2c210ed0582b2cfc66e7a11e34f380
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-07-07 14:30:02 +0530

    remove test page
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- src/app/test-page/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Calendar } from "@/components/ui/calendar";
-
-export default function TestPage() {
-  return (
-    <main>
-      <Calendar captionLayout="dropdown-buttons" fromYear={1990} toYear={2021} />
-    </main>
-  );
-}
